15ab0cd84b28ba1cf45d670ef42cd02e8fe646e9
vtanguturi <vtanguturi3@gatech.edu>
2018-08-26 12:36:17 -0400

    Create level1.py

---
+++ level1.py
@@ -0,0 +1,3 @@
+def answer(x, y):
+# This is the one where you had to find an extra number in the other list
+# Check whichever is longer, if that number does not exist in the other list then you return that one
